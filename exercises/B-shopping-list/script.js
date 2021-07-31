6d2590303b41747cf9e9a6d6274f0a094bd37216
Abed <aalag230@gmail.com>
2021-07-31 12:05:34 +0100

    update

--- exercises/B-shopping-list/script.js
+++ exercises/B-shopping-list/script.js
@@ -1,5 +1,18 @@
 function shoppingList(arrayOfPeople) {
   // Write your code here...
+  function shoppingList(arrayItems) {
+    let content = document.getElementById('content');
+    let unorderList = document.createElement('ul');
+
+    //li
+    shoppingItems.forEach(el => {
+      let itemsListed = document.createElement('li');
+      itemsListed.innerHTML = el;
+
+      content.appendChild(list);
+      unorderList.appendChild(itemsListed);
+    });
+  }
 }
 
 let shopping = ["Milk", "Bread", "Eggs", "A Dinosaur", "Cake", "Sugar", "Tea"];
